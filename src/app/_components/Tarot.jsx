246f2a3cb1b22ad3d59fbdb8449688da58f52b59
vtyeh <vtyeh24@gmail.com>
2024-07-31 14:42:02 -0700

    use reactcardflip component, resize cards

--- src/app/_components/Tarot.jsx
+++ src/app/_components/Tarot.jsx
@@ -1,25 +1,91 @@
+import { useRouter, useParams } from "next/navigation";
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import Image from "next/image";
 import Message from "./Message";
+import ReactCardFlip from "react-card-flip";
+import { useUser } from "../context/UserContext";
 
-export default function Tarot({ gifProps, quote, author }) {
+export default function Tarot() {
+  const { name } = useUser();
+  const { cardId } = useParams();
+
+  const [flipped, setFlipped] = useState(false);
+
+  const [quote, setQuote] = useState(null);
+  const [author, setAuthor] = useState(null);
+  const [gifProps, setGifProps] = useState(null);
+
+  function resizeImage(height, width) {
+    if (width > 355) {
+      const aspectRatio = height / width;
+      width = 355;
+      height = width * aspectRatio;
+    }
+    return { height, width };
+  }
+
+  useEffect(() => {
+    async function fetchGifAndQuote() {
+      try {
+        const quoteResponse = await fetch(`/api/quote`);
+        const quoteData = await quoteResponse.json();
+        setQuote(quoteData.data[0].q);
+        setAuthor(quoteData.data[0].a);
+
+        const gifResponse = await fetch(`/api/gif`);
+        const gifData = await gifResponse.json();
+        const randomIndex = Math.floor(Math.random() * 10); // multiplier should equal whatever the limit is in api/gif query
+        const originalGif = gifData.data.data[randomIndex].images.original;
+        const imgDimension = resizeImage(originalGif.height, originalGif.width);
+
+        setGifProps({
+          url: originalGif.url,
+          height: imgDimension.height,
+          width: imgDimension.width,
+        });
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    }
+
+    if (name && cardId) {
+      fetchGifAndQuote();
+    }
+  }, [name, cardId]);
+
+  useEffect(() => {
+    if (gifProps) {
+      const timer = setTimeout(() => {
+        setFlipped(true);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [gifProps]);
+
   return (
     <>
       <Message />
-      <div id="tarot">
-        {gifProps && (
-          <Image
-            src={gifProps.url}
-            width={gifProps.width}
-            height={gifProps.height}
-            alt="random gif"
-          />
-        )}
-        <p id="quote">
-          {quote}
-          <br />- {author}
-        </p>
-      </div>
+      <ReactCardFlip isFlipped={flipped}>
+        <div className="tarot back">
+          <div className="tarot-inner-border"></div>
+        </div>
+        <div className="tarot front">
+          {gifProps && (
+            <Image
+              src={gifProps.url}
+              width={gifProps.width}
+              height={gifProps.height}
+              alt="random gif"
+            />
+          )}
+          <p id="quote">
+            {quote}
+            <br />- {author}
+          </p>
+        </div>
+      </ReactCardFlip>
       <Button label="Reshuffle" routeName="/intention" />
     </>
   );
--- src/app/reading/[cardId]/page.js
+++ src/app/reading/[cardId]/page.js
@@ -1,63 +1,17 @@
 "use client";
-import { useRouter, useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import { useUser } from "../../context/UserContext";
 import Tarot from "../../_components/Tarot";
 
 export default function Reading() {
-  let { name, setName } = useUser();
-  let [quote, setQuote] = useState(null);
-  let [author, setAuthor] = useState(null);
-  let [gifProps, setGifProps] = useState(null);
-  let { cardId } = useParams();
   const router = useRouter();
 
-  function resizeImage(height, width) {
-    if (width > 400) {
-      const aspectRatio = height / width;
-      width = 400;
-      height = width * aspectRatio;
-    }
-    return { height, width };
-  }
-
-  useEffect(() => {
-    async function fetchGifAndQuote() {
-      try {
-        const quoteResponse = await fetch(`/api/quote`);
-        const quoteData = await quoteResponse.json();
-        setQuote(quoteData.data[0].q);
-        setAuthor(quoteData.data[0].a);
-
-        const gifResponse = await fetch(`/api/gif`);
-        const gifData = await gifResponse.json();
-        const randomIndex = Math.floor(Math.random() * 10); // multiplier should equal whatever the limit is in api/gif query
-        const originalGif = gifData.data.data[randomIndex].images.original;
-        const imgDimension = resizeImage(originalGif.height, originalGif.width);
-
-        setGifProps({
-          url: originalGif.url,
-          height: imgDimension.height,
-          width: imgDimension.width,
-        });
-      } catch (error) {
-        console.error("Error fetching data", error);
-      }
-    }
-
-    if (name && cardId) {
-      fetchGifAndQuote();
-    }
-  }, [name, cardId]);
+  const { name } = useUser();
 
   return (
     <div className="center-screen">
-      {name ? (
-        <Tarot gifProps={gifProps} quote={quote} author={author} />
-      ) : (
-        <p>Who are you?</p>
-      )}
+      {name ? <Tarot /> : <p>Who are you?</p>}
       <div
         className="home-button"
         onClick={() => {
